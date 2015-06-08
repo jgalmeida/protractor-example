9c255523a1e7e790367909bb2316209d3d740739
João Almeida <jg.almeida56@gmail.com>
2015-06-08 19:55:30 +0100

    Added test suites

--- todo/protractor_conf.js
+++ todo/protractor_conf.js
@@ -13,12 +13,20 @@ exports.config = {
 
   framework: 'jasmine2',
 
-  specs: ['test/e2e/**/*test.js'],
-    getPageTimeout: 10000,
-    allScriptsTimeout: 500000,
+  //specs: ['test/e2e/**/*test.js'],
+  suites: {
+    layout: 'test/e2e/layout.test.js',
+    signin: 'test/e2e/signin.test.js',
+    todos:  'test/e2e/todos.test.js'
+  },
 
-    jasmineNodeOpts: {
-      showColors: true,
-      defaultTimeoutInterval: 150000
-    }
+  getPageTimeout: 10000,
+  allScriptsTimeout: 500000,
+
+
+
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 150000
+  }
 };
