1bc396a18f737cef5b3d56da0e491284457b1ad3
João Almeida <jg.almeida56@gmail.com>
2015-06-08 20:56:21 +0100

    Prepare

--- todo/test/e2e/signin.test.js
+++ todo/test/e2e/signin.test.js
@@ -1,33 +1,14 @@
 describe('Signin', function() {
-  var layoutPage = require('./page_objects/layout.po');
-  var homePage   = require('./page_objects/home.po');
-  var signinPage = require('./page_objects/signin.po');
-
   beforeAll(function() {
     browser.get(global.basePath);
   });
 
   it('navigate to signin page', function() {
-    layoutPage.getTab('Sign in').click();
-
-    expect(signinPage.getUsername.isPresent()).toBeTruthy();
-    expect(signinPage.getSubmitBtn().isPresent()).toBeTruthy();
   });
 
   it('sign in a user', function() {
-    signinPage.setUsername('Jony yld');
-    signinPage.submit();
-
-    expect(browser.getCurrentUrl()).toBe(global.basePath + '/#/');
-    expect(layoutPage.getCurrentUsername()).toBe('Jony yld');
-    expect(homePage.getGreetings()).toBe('Hello Jony yld');
   });
 
   it('log out a user', function() {
-    var logout = layoutPage.getTab('Logout');
-
-    expect(logout.isPresent()).toBeTruthy();
-    logout.click()
-    expect(layoutPage.getCurrentUser.isPresent()).toBeFalsy();
   });
 });
--- todo/test/e2e/todos.test.js
+++ todo/test/e2e/todos.test.js
@@ -1,6 +1,4 @@
 describe('Todos', function() {
-  var todoPage = require('./page_objects/todo.po');
-
   beforeAll(function() {
     require('./helpers/auth').signin('Jony yld');
     browser.get(global.basePath + '#/todos');
@@ -10,64 +8,23 @@ describe('Todos', function() {
   });
 
   it('Adds 2 todos', function() {
-    todoPage.addTodo('First');
-    todoPage.addTodo('Second');
-
-    expect(todoPage.todosCount()).toBe(2);
-    expect(todoPage.getTodo(0).description).toBe('First');
-    expect(todoPage.getTodo(1).description).toBe('Second');
-    expect(todoPage.getRemaining()).toBe('Remaining 2');
   });
 
   it('Marks a todo as completed', function() {
-    todoPage.getTodo(0).complete();
-
-    expect(todoPage.getRemaining()).toBe('Remaining 1');
-    expect(todoPage.getCompleted()).toBe('Completed 1');
   });
 
   it('Removes both todos', function() {
-    todoPage.getTodo(0).remove();
-    todoPage.getTodo(0).remove();
-
-    expect(todoPage.getRemaining()).toBe('Remaining 0');
-    expect(todoPage.getCompleted()).toBe('Completed 0');
   });
 
   it('Adds 5 Marks all todos as completed', function() {
-    var todos = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
-    todos.forEach(todoPage.addTodo);
-
-    todoPage.markAllCompleted();
-
-    expect(todoPage.getRemaining()).toBe('Remaining 0');
-    expect(todoPage.getCompleted()).toBe('Completed 5');
   });
 
   it('Removes all todos', function() {
-    todoPage.clearCompleted();
-
-    expect(todoPage.getCompleted()).toBe('Completed 0');
   });
 
   it('Adds 5 todos, marks first two as completed and filters active todos', function() {
-    var todos = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
-    todos.forEach(todoPage.addTodo);
-
-    todoPage.getTodo(0).complete();
-    todoPage.getTodo(1).complete();
-
-    expect(todoPage.getRemaining()).toBe('Remaining 3');
-    expect(todoPage.getCompleted()).toBe('Completed 2');
-
-    todoPage.filterActive();
-
-    expect(todoPage.todosCount()).toBe(3);
   });
 
   it('filters completed todos', function() {
-    todoPage.filterCompleted();
-
-    expect(todoPage.todosCount()).toBe(2);
   });
 });
